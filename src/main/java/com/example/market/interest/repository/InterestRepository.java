55e7b465c82131452cb6e1a0f4b4ce30756b2bd6
wltngus200 <wltngus200@gmail.com>
2024-10-02 15:51:56 +0900

    1002 JSH Entity

--- src/main/java/com/example/market/interest/repository/InterestRepository.java
+++ src/main/java/com/example/market/interest/repository/InterestRepository.java
@@ -1,6 +1,6 @@
 package com.example.market.interest.repository;
 
-import com.example.market.interest.repository.entity.Interest;
+import com.example.market.entity.Interest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InterestRepository extends JpaRepository<Interest, Long> {
