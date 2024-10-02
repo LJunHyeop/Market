e1a518a7cca9ac1dfb2f71789e399fe9ed798cec
LJM <tg03067@gmail.com>
2024-10-02 16:16:36 +0900

    1234

--- src/main/java/com/example/market/product/ProductController.java
+++ src/main/java/com/example/market/product/ProductController.java
@@ -1,8 +1,13 @@
 package com.example.market.product ;
 
+import org.apache.ibatis.javassist.tools.framedump;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
@@ -13,5 +18,9 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("api/product")
 @Tag(name="상품", description="상품 CRUD")
 public class ProductController {
+    private final ProductService ;
 
+    // @PostMapping("/registration")
+    // @Operation(summary = "상품등록")
+    // public ResponseEntity postProduct(@RequestBody )
 }
---
+++ src/main/java/com/example/market/product/model/ProductRegistrationReq.java
@@ -0,0 +1,5 @@
+package com.example.market.product.model;
+
+public class ProductRegistrationReq {
+    
+}
