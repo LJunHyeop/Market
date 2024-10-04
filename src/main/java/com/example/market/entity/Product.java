7bfc8a5b385f6e06f07bba126cf7023488361cc5
miss-gif <svx327@naver.com>
2024-10-04 15:56:53 +0900

    Setting

--- src/main/java/com/example/market/entity/Product.java
+++ src/main/java/com/example/market/entity/Product.java
@@ -1,15 +1,8 @@
 package com.example.market.entity;
 
+import jakarta.persistence.*;
 import org.hibernate.annotations.Comment ;
 
-import jakarta.persistence.Column ;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,7 +16,7 @@ public class Product extends UpdateAt {
     private long productPk ;
 
     @JoinColumn(name = "user_pk", nullable = false)
-    @Comment("유저 FK")
+    @Comment("유저 PK")
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
--- src/main/java/com/example/market/report/ReportController.java
+++ src/main/java/com/example/market/report/ReportController.java
@@ -1,9 +1,13 @@
 package com.example.market.report;
 
+import com.example.market.report.model.GetReportReq;
+import com.example.market.report.model.GetReportRes;
+import com.example.market.report.model.PostReport;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -12,4 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReportController {
     private final ReportService service;
 
+
+    //신고 하는 컨트롤러
+    @PostMapping("/post")
+    public int postReport(@RequestBody PostReport p){
+        return  service.postReport(p);
+    }
+    //신고 전체리스트 조회 컨트롤러
+    @GetMapping("/Get/List")
+    public List<GetReportReq> getReportList(@RequestParam GetReportRes p){
+        return  service.GetReportList(p);
+    }
+    @GetMapping("/Get/User")
+    public List<GetReportReq> getReportUser(@RequestParam GetReportRes p){
+        return service.GetReport(p);
+    }
 }
