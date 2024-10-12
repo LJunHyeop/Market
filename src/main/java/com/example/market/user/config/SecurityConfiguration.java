f780e5671c676a05ff6343da2cf35b4b27d98260
이진현 <ljun88@naver.com>
2024-10-12 14:56:56 +0900

    토큰보내기중

--- src/main/java/com/example/market/user/config/SecurityConfiguration.java
+++ src/main/java/com/example/market/user/config/SecurityConfiguration.java
@@ -26,16 +26,14 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
+
+
         return httpSecurity
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Stateless 세션 유지
                 .httpBasic(http -> http.disable()) // HTTP Basic 인증 비활성화
                 .formLogin(form -> form.disable()) // Form 로그인 비활성화
                 .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/swagger/**", "/v3/api-docs/**").permitAll()
-                        .requestMatchers("/api/user/sign-up").permitAll() // 회원가입 API는 익명 사용자도 접근 가능하게 설정
-                        .requestMatchers( "api/user**", "api/user/**" ).authenticated() // /api/user 하위 경로는 인증 필요
-                        .requestMatchers("/api/admin/**").hasRole("ADMIN") // /api/admin/** 경로는 ADMIN 권한 필요
                         .anyRequest().permitAll() // 그 외 모든 요청은 허용
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
